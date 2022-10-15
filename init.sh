15ed4175ec8960d5ea24d81658ef9ce483bb59e7
Avermakov <avermakov@list.ru>
2022-10-15 14:36:18 +0600

    Rename files

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn application.wsgi --bind 0.0.0.0:8000
+gunicorn web.hello --bind 0.0.0.0:8000

fdae728dc4ae24267e4abddbb3d214f81e0a9f6a
Avermakov <avermakov@list.ru>
2022-10-15 22:04:14 +0600

    Try again

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 gunicorn web.hello --bind 0.0.0.0:8080

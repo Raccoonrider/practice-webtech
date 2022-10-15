9e3cbccbbd7f7f7998c68831d3ba1001cd1d0f99
Avermakov <avermakov@list.ru>
2022-10-15 23:27:57 +0600

    Downgrade to django 1.9 :(

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 gunicorn web.hello --bind 0.0.0.0:8080 &
-cd ask
+cd web/ask
 gunicorn ask.wsgi --bind 0.0.0.0:8000 &
